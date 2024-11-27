03a57a1a7677e396679a8cc9b1a89b34ba4de383
serhiipukha <serhii.pukha@persoo.cz>
2024-11-27 15:07:26 +0100

    remove components that are already in use

--- components/landing-page/scene.js
+++
@@ -1,8 +0,0 @@
-const images = ["44.jpg", "33.jpg", "55.jpg"];
-let currentImageIndex = 0;
-const mirrorContent = document.getElementById("mirror-content");
-
-mirrorContent.addEventListener("click", () => {
-	currentImageIndex = (currentImageIndex + 1) % images.length;
-	mirrorContent.style.backgroundImage = `url(${images[currentImageIndex]})`;
-});
